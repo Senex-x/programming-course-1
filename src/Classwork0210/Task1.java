eb93f6573afd6390ae2ebe5251ac2318bb597126
Senex-V <vdm.snx@gmail.com>
2020-10-02 10:25:06 +0300

    Update 1

---
+++ src/Classwork0210/Task1.java
@@ -0,0 +1,11 @@
+package Classwork0210;
+
+import java.util.*;
+
+public class Task1 {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
