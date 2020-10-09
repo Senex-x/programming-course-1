720fb7cd289ec6c9b4226aba7948aba96e0add93
Senex-V <vdm.snx@gmail.com>
2020-10-09 11:47:54 +0300

    Update 5
    Made tasks 1-4 for HW 16.10

---
+++ src/HomeworkTo1610/Task1.java
@@ -0,0 +1,19 @@
+package HomeworkTo1610;
+
+import Methods.Methods;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int x = Methods.getInt();
+        System.out.println(ranks(x));
+    }
+
+    private static int ranks(int num) {
+        int counter = 0;
+        while(num != 0) {
+            counter++;
+            num/=10;
+        }
+        return counter;
+    }
+}
