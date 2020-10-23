84df17c99d982bc2a734cc88798359cc799bfeab
Senex-V <vdm.snx@gmail.com>
2020-10-23 10:29:26 +0300

    Local project updated

--- src/HomeworkTo2310/Task3.java
+++ src/HomeworkTo2310/Task3.java
@@ -4,19 +4,18 @@ import Methods.Methods;
 
 /**
  * ATTENTION!
- * numeration from zero
+ * numeration from one
  */
 
 public class Task3 {
     public static void main(String[] args) {
         int n = Methods.getInt();
-        System.out.println(fibonnacci(n - 1));
+        System.out.println(fibonnacci(n));
     }
 
     private static int fibonnacci(int n) {
-        //System.out.print(n + " ");
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+        if (n == 1) return 0;
+        if (n == 2) return 1;
         return fibonnacci(n - 1) + fibonnacci(n - 2);
     }
 }
