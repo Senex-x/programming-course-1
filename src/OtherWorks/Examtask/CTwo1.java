e8eb85e3ca763b17e9e636f52adc6f8ad98efa03
V-Senex <vdm.snx@gmail.com>
2020-11-05 13:36:26 +0300

    Exam solution added

---
+++ src/OtherWorks/Examtask/CTwo1.java
@@ -0,0 +1,33 @@
+package OtherWorks.CoffeeMashine.examtask;
+
+import java.util.*;
+
+public class CTwo1 {
+    public static int getI(int k, int m) {
+        return k;
+    }
+
+    public static int getJ(int k, int m, int n) {
+        return n - k - m - 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] a = new int[n][n];
+
+        int m = sc.nextInt();
+        for (int k = 0; k < n - m; k++) {
+            a[getI(k, m)][getJ(k, m, n)] = k + 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
