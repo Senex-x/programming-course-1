e1e3d6f2a423765e92f4a4735ee9e7a76cc56dd5
V-Senex <vdm.snx@gmail.com>
2020-11-25 15:52:22 +0300

    Containers test 1 added

---
+++ src/OtherWorks/Samples/Containers1.java
@@ -0,0 +1,68 @@
+package OtherWorks.Samples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
+
+public class Containers1 {
+    static ArrayList<? extends Item> items = new ArrayList<>(
+            Arrays.asList(
+                    new Item(),
+                    new ItemOne(),
+                    new ItemTwo()
+            )
+    );
+    static ArrayList<? extends Item> itemsOne = new ArrayList<>(
+            Arrays.asList(
+                    new ItemOne(),
+                    new ItemOne(),
+                    new ItemOne()
+            )
+    );
+    static ArrayList<? extends Item> itemsTwo = new ArrayList<>(
+            Arrays.asList(
+                    new ItemTwo(),
+                    new ItemTwo(),
+                    new ItemTwo()
+            )
+    );
+
+    public static void main(String[] args) {
+        displayAllItems(itemsOne);
+        displayAllItems(itemsTwo);
+
+        System.out.println(items);
+        ListIterator<? extends Item> it = items.listIterator();
+        while(it.hasNext()) {
+            System.out.println(it.previousIndex() + " " + it.nextIndex());
+            it.next().display();
+        }
+    }
+
+    static void displayAllItems(ArrayList<? extends Item> array) {
+        for(Item item : array) {
+            item.display();
+        }
+    }
+
+}
+
+class Item {
+    void display() {
+        System.out.println("Item display");
+    }
+}
+
+class ItemOne extends Item {
+    @Override
+    void display() {
+        System.out.println("ItemOne display");
+    }
+}
+
+class ItemTwo extends Item {
+    @Override
+    void display() {
+        System.out.println("ItemTwo display");
+    }
+}
