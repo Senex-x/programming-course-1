ee04b3aa4b3af16ef72b02714ff3bceba7d9ec70
V-Senex <vdm.snx@gmail.com>
2020-11-27 19:23:40 +0300

    Exceptions test updated

--- src/OtherWorks/Samples/Exceptions1.java
+++ src/OtherWorks/Samples/Exceptions1.java
@@ -3,15 +3,24 @@ package OtherWorks.Samples;
 public class Exceptions1 {
     public static void main(String[] args) {
         try {
-            throwException();
-            throwException();
+            //throwException(new ClassCastException());
+            throwException(new MyException("MyException"));
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Handling exception: " + e.getClass().getSimpleName());
         }
+        System.out.println("Continue work...");
     }
 
-    static void throwException() throws Exception {
-        System.out.println("Throwing exception");
-        throw new Exception();
+    static void throwException(Exception exception) throws Exception {
+        System.out.println("Throwing exception: " + exception.getClass().getSimpleName());
+        throw exception;
+    }
+
+    static class MyException extends Exception {
+        private String desc;
+
+        MyException(String desc) {
+            this.desc = desc;
+        }
     }
 }
