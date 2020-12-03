f21903c8021fef7d5a4e0579a7c51a09054d107f
V-Senex <junior.07.inator@gmail.com>
2020-12-03 21:51:54 +0300

    RailwayProject DatabaseHandler class added

---
+++ src/Homeworks/Month12/RailwayProject/DatabaseHandler.java
@@ -0,0 +1,9 @@
+package Homeworks.Month12.RailwayProject;
+
+public class DatabaseHandler {
+    private static final String PATH = "D:\\Projects\\Java\\PolyakovV_11005\\src\\Homeworks\\Month12\\RailwayProject\\data";
+
+    public static void add(Train train) {
+
+    }
+}
