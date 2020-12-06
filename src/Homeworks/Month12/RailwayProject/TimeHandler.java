0adf85029d883be0023cc61d0ccbed6ef4744c92
V-Senex <vdm.snx@gmail.com>
2020-12-06 14:11:04 +0300

    RailwayProject TimeHandler class changed

---
+++ src/Homeworks/Month12/RailwayProject/TimeHandler.java
@@ -0,0 +1,52 @@
+package Homeworks.Month12.RailwayProject;
+
+import static Methods.Methods.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+class TimeHandler {
+    private final GregorianCalendar calendar;
+    private final SimpleDateFormat formatter;
+    private String formattedDate;
+
+    TimeHandler() {
+        Calendar currentDate = Calendar.getInstance();
+        calendar = new GregorianCalendar(
+                currentDate.get(Calendar.YEAR),
+                currentDate.get(Calendar.MONTH),
+                currentDate.get(Calendar.DAY_OF_MONTH));
+        formatter = new SimpleDateFormat("HH 'ч.' dd MMMM yyyy", Locale.US);
+        updateFormattedDate();
+        printDate();
+    }
+
+    void nextHour() {
+        calendar.add(Calendar.HOUR, 1);
+        updateFormattedDate();
+        printDate();
+    }
+
+    void printDate() {
+        System.out.println(paint(Colors.GREEN,"#####   CURRENT DATE: " + formattedDate + "   #####"));
+    }
+
+    void updateFormattedDate() {
+        formattedDate = formatter.format(calendar.getTime());
+    }
+
+    int getHour() {
+        return calendar.get(Calendar.HOUR);
+    }
+
+    int getDay() {
+        return calendar.get(Calendar.MONTH);
+    }
+
+    @Override
+    public String toString() {
+        return formattedDate;
+    }
+}
