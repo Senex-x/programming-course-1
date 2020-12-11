7896b3c25312122bf977dc7757b2b86477d085ec
V-Senex <vdm.snx@gmail.com>
2020-12-11 21:41:48 +0300

    RailwayProject Tickets logic updated

--- src/Homeworks/Month12/RailwayProject/Passenger.java
+++ src/Homeworks/Month12/RailwayProject/Passenger.java
@@ -7,16 +7,33 @@ public class Passenger {
     private String name;
     private String password;
     private Ticket currentTicket;
-    private ArrayList<Ticket> tripsHistory;
+    private HistoryHolder historyHolder;
 
     public Passenger(int id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
+        historyHolder = new HistoryHolder();
     }
 
     void buyTicket(Ticket ticket) {
+        historyHolder.addToHistory(ticket);
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<Ticket> getTripsHistory() {
+        return historyHolder.getTripsHistory();
     }
 
     @Override
@@ -30,8 +47,18 @@ public class Passenger {
     class TicketHandler {
         Ticket currentTicket;
 
-        class HistoryHolder {
-            ArrayList<Ticket> tripsHistory;
+
+    }
+
+    static class HistoryHolder {
+        ArrayList<Ticket> tripsHistory = new ArrayList<>();
+
+        void addToHistory(Ticket ticket) {
+            tripsHistory.add(ticket);
+        }
+
+        public ArrayList<Ticket> getTripsHistory() {
+            return tripsHistory;
         }
     }
 
--- src/Homeworks/Month12/RailwayProject/Ticket.java
+++ src/Homeworks/Month12/RailwayProject/Ticket.java
@@ -1,10 +1,16 @@
 package Homeworks.Month12.RailwayProject;
 
 public class Ticket {
-    private Passenger passenger;
+    private int passengerId;
     private int cost;
-    private Train train;
-    private String date;
+    private int trainId;
+    private String dateOfDeparture;
+    private String dateOfDestination;
     private Station departure;
     private Station destination;
+
+    public Ticket(int passengerId, int cost) {
+        this.passengerId = passengerId;
+        this.cost = cost;
+    }
 }
