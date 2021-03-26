93156ee15edbbfd0dd2dea32b335a078dfc60736
V-Senex <junior.07.inator@gmail.com>
2021-03-26 17:34:01 +0300

    MarkovChain bugurt generation support added

--- src/OtherWorks/Projects/MarkovChain/MarkovChain.java
+++ src/OtherWorks/Projects/MarkovChain/MarkovChain.java
@@ -1,23 +1,49 @@
 package OtherWorks.Projects.MarkovChain;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
+import java.util.*;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
 
 public class MarkovChain {
-    private static final String PATH = "src/OtherWorks/Projects/MarkovChain/data/text.txt";
+    private static final String PATH_TEXT1 = "src/OtherWorks/Projects/MarkovChain/data/text1.txt";
+    private static final String PATH_TEXT2 = "src/OtherWorks/Projects/MarkovChain/data/text2.txt";
 
     public static void main(String[] args) {
         WordsHandler wordsHandler = new WordsHandler();
-        wordsHandler.parseFile(PATH);
+        wordsHandler.parseFile(PATH_TEXT2);
         wordsHandler.getText(500);
+
+        //WordsHandler.parseExcelTo("src/OtherWorks/Projects/MarkovChain/data/bugurts.xls", PATH_TEXT2);
     }
+
 
     static class WordsHandler {
         ArrayList<Word> words = new ArrayList<>();
         String base;
+
+        static void clearText(String path) {
+            StringBuilder text = new StringBuilder();
+            try (BufferedReader buf = new BufferedReader(new FileReader(path))) {
+                String line;
+                while ((line = buf.readLine()) != null) {
+                    String[] words = line.split(" ");
+                    for (int i = 0; i < words.length; i++) {
+                        if (!words[i].contains(";") && !words[i].contains("http") && !words[i].contains("#")) {
+                           text.append(words[i] + " ");
+                        }
+                    }
+                    text.append("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            writeTo(text.toString(), path);
+        }
+
 
         void parseFile(String path) {
             try (BufferedReader buf = new BufferedReader(new FileReader(path))) {
@@ -30,12 +56,45 @@ public class MarkovChain {
                             addFirstBase(word);
                             firstTime = false;
                         } else {
-                            // parser crutch
-                            if (!word.contains("[") && !word.contains("]"))
-                                addWord(word);
+                            addWord(word);
                         }
                     }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public static void parseExcelTo(String path, String file) {
+            try {
+                HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(path));
+                HSSFSheet myExcelSheet = myExcelBook.getSheet("Worksheet");
+                StringBuilder text = new StringBuilder();
+                int count = 0;
+                while (true) {
+                    HSSFRow row = myExcelSheet.getRow(count++);
+                    if (row != null && row.getCell(0) != null && row.getCell(0).getCellType() == CellType.STRING) {
+                        text.append(row.getCell(0).getStringCellValue() + "\n");
+                        System.out.println(count);
+                        continue;
+                    }
+                    if (row != null && row.getCell(0) != null && row.getCell(0).getCellType() == CellType.NUMERIC) {
+                        text.append(row.getCell(0).getNumericCellValue() + "\n");
+                        System.out.println(count);
+                        continue;
+                    }
+                    break;
+                }
+                writeTo(text.toString(), file);
+                myExcelBook.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        static void writeTo(String text, String path) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+                writer.write(text);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -82,7 +141,11 @@ public class MarkovChain {
             String word = "";
             for (int i = 1; i < length + 1 && word != null; i++) {
                 word = getNext();
-                System.out.print(word + " ");
+                if(word.contains("@")) {
+                    System.out.println("\n" + word);
+                } else {
+                    System.out.print(word + " ");
+                }
                 if (i % 10 == 0) {
                     System.out.println();
                 }
